5bcb9b01c36efef856c8f1e50296ec028ab54de7
Stephane Letz <letz@grame.fr>
2020-01-11 11:19:10 +0100

    Move class in esp32.cpp file, remove esp32.h one.

--- architecture/esp32/esp32.cpp
+++ architecture/esp32/esp32.cpp
@@ -9,7 +9,7 @@
 
 /************************************************************************
  FAUST Architecture File
- Copyright (C) 2003-2019 GRAME, Centre National de Creation Musicale &
+ Copyright (C) 2003-2020 GRAME, Centre National de Creation Musicale &
  Aalborg University (Copenhagen, Denmark)
  ---------------------------------------------------------------------
  This Architecture section is free software; you can redistribute it
@@ -33,7 +33,9 @@
  ************************************************************************
  ************************************************************************/
 
-#include "esp32.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/i2s.h"
 
 #include "faust/gui/meta.h"
 #include "faust/dsp/dsp.h"
@@ -77,181 +79,190 @@
 #define DIV_S32 4.6566129e-10
 #define clip(sample) std::max(-MULT_S32, std::min(MULT_S32, ((int32_t)(sample * MULT_S32))));
 
-AudioFaust::AudioFaust(int sample_rate, int buffer_size)
+class AudioFaust
 {
-    fBS = buffer_size;
-    fDSP = new mydsp();
-    fDSP->init(sample_rate);
-    fUI = new MapUI();
-    fDSP->buildUserInterface(fUI);
-    fHandle = NULL;
+    private:
     
-    i2s_pin_config_t pin_config;
-#if TTGO_TAUDIO
-    pin_config = {
-        .bck_io_num = 33,
-        .ws_io_num = 25,
-        .data_out_num = 26,
-        .data_in_num = 27
-    };
-#elif A1S_BOARD
-    pin_config = {
-        .bck_io_num = 27,
-        .ws_io_num = 26,
-        .data_out_num = 25,
-        .data_in_num = 35
-    };
-#else // Default
-    pin_config = {
-        .bck_io_num = 33,
-        .ws_io_num = 25,
-        .data_out_num = 26,
-        .data_in_num = 27
-    };
-#endif
-    configureI2S(sample_rate, buffer_size, pin_config);
-
-    if (fDSP->getNumInputs() > 0) {
-        fInChannel = new float*[fDSP->getNumInputs()];
-        for (int i = 0; i < fDSP->getNumInputs(); i++) {
-            fInChannel[i] = new float[fBS];
+        mydsp fDSP;
+        MapUI fUI;
+        float **fInChannel, **fOutChannel;
+        int fBS;
+        TaskHandle_t fHandle;
+    
+        template <int INPUTS, int OUTPUTS>
+        void audioTask()
+        {
+            while (true) {
+                if (INPUTS > 0) {
+                    // Read from the card
+                    int32_t samples_data_in[2*fBS];
+                    size_t bytes_read = 0;
+                    i2s_read((i2s_port_t)0, &samples_data_in, 8*fBS, &bytes_read, portMAX_DELAY);
+                    
+                    // Convert and copy inputs
+                    if (INPUTS == 2) {
+                        // if stereo
+                        for (int i = 0; i < fBS; i++) {
+                            fInChannel[0][i] = (float)samples_data_in[i*2]*DIV_S32;
+                            fInChannel[1][i] = (float)samples_data_in[i*2+1]*DIV_S32;
+                        }
+                    } else {
+                        // otherwise only first channel
+                        for (int i = 0; i < fBS; i++) {
+                            fInChannel[0][i] = (float)samples_data_in[i*2]*DIV_S32;
+                        }
+                    }
+                }
+                
+                // Call DSP
+                fDSP.compute(fBS, fInChannel, fOutChannel);
+                
+                // Convert and copy outputs
+                int32_t samples_data_out[2*fBS];
+                if (OUTPUTS == 2) {
+                    // if stereo
+                    for (int i = 0; i < fBS; i++) {
+                        samples_data_out[i*2] = clip(fOutChannel[0][i]);
+                        samples_data_out[i*2+1] = clip(fOutChannel[1][i]);
+                    }
+                } else {
+                    // otherwise only first channel
+                    for (int i = 0; i < fBS; i++) {
+                        samples_data_out[i*2] = clip(fOutChannel[0][i]);
+                        samples_data_out[i*2+1] = samples_data_out[i*2];
+                    }
+                }
+                
+                // Write to the card
+                size_t bytes_written = 0;
+                i2s_write((i2s_port_t)0, &samples_data_out, 8*fBS, &bytes_written, portMAX_DELAY);
+            }
         }
-    } else {
-        fInChannel = NULL;
-    }
     
-    if (fDSP->getNumOutputs() > 0) {
-        fOutChannel = new float*[fDSP->getNumOutputs()];
-        for (int i = 0; i < fDSP->getNumOutputs(); i++) {
-            fOutChannel[i] = new float[fBS];
+        static void audioTaskHandler(void* arg)
+        {
+            AudioFaust* audio = (AudioFaust*)arg;
+            audio->audioTask<FAUST_INPUTS, FAUST_OUTPUTS>();
         }
-    } else {
-        fOutChannel = NULL;
-    }
-}
-
-AudioFaust::~AudioFaust()
-{
-    for (int i = 0; i < fDSP->getNumInputs(); i++) {
-        delete[] fInChannel[i];
-    }
-    delete [] fInChannel;
     
-    for (int i = 0; i < fDSP->getNumOutputs(); i++) {
-        delete[] fOutChannel[i];
-    }
-    delete [] fOutChannel;
-    
-    delete fDSP;
-    delete fUI;
-}
-
-bool AudioFaust::start()
-{
-    xTaskCreate(audioTaskHandler, "Faust DSP Task", 1024, (void*)this, 5, &fHandle);
-    return true;
-}
-
-void AudioFaust::stop()
-{
-    if (fHandle != NULL) {
-        vTaskDelete(fHandle);
-        fHandle = NULL;
-    }
-}
-
-void AudioFaust::setParamValue(const std::string& path, float value)
-{
-    fUI->setParamValue(path, value);
-}
-
-void AudioFaust::configureI2S(int sample_rate, int buffer_size, i2s_pin_config_t pin_config)
-{
+        void configureI2S(int sample_rate, int buffer_size, i2s_pin_config_t pin_config)
+        {
     #if A1S_BOARD
-    i2s_config_t i2s_config = {
-        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_RX),
-        .sample_rate = sample_rate,
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // high interrupt priority
-        .dma_buf_count = 3,
-        .dma_buf_len = buffer_size,
-        .use_apll = true
-    };
+            i2s_config_t i2s_config = {
+                .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_RX),
+                .sample_rate = sample_rate,
+                .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+                .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+                .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+                .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // high interrupt priority
+                .dma_buf_count = 3,
+                .dma_buf_len = buffer_size,
+                .use_apll = true
+            };
     #else // default
-        i2s_config_t i2s_config = {
-        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_RX),
-        .sample_rate = sample_rate,
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // high interrupt priority
-        .dma_buf_count = 3,
-        .dma_buf_len = buffer_size,
-        .use_apll = false
-    };
+            i2s_config_t i2s_config = {
+                .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_RX),
+                .sample_rate = sample_rate,
+                .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+                .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+                .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+                .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // high interrupt priority
+                .dma_buf_count = 3,
+                .dma_buf_len = buffer_size,
+                .use_apll = false
+            };
     #endif
-    i2s_driver_install((i2s_port_t)0, &i2s_config, 0, NULL);
-    i2s_set_pin((i2s_port_t)0, &pin_config);
-    PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0_CLK_OUT1);
-    REG_WRITE(PIN_CTRL, 0xFFFFFFF0);
-}
-
-template <int INPUTS, int OUTPUTS>
-void AudioFaust::audioTask()
-{
-    while (true) {
-        if (INPUTS > 0) {
-            // Read from the card
-            int32_t samples_data_in[2*fBS];
-            size_t bytes_read = 0;
-            i2s_read((i2s_port_t)0, &samples_data_in, 8*fBS, &bytes_read, portMAX_DELAY);
+            i2s_driver_install((i2s_port_t)0, &i2s_config, 0, NULL);
+            i2s_set_pin((i2s_port_t)0, &pin_config);
+            PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0_CLK_OUT1);
+            REG_WRITE(PIN_CTRL, 0xFFFFFFF0);
+        }
+    
+    public:
+    
+        AudioFaust(int sample_rate, int buffer_size)
+        {
+            fBS = buffer_size;
+            fDSP.init(sample_rate);
+            fDSP.buildUserInterface(&fUI);
+            fHandle = NULL;
             
-            // Convert and copy inputs
-            if (INPUTS == 2) {
-                // if stereo
-                for (int i = 0; i < fBS; i++) {
-                    fInChannel[0][i] = (float)samples_data_in[i*2]*DIV_S32;
-                    fInChannel[1][i] = (float)samples_data_in[i*2+1]*DIV_S32;
+            i2s_pin_config_t pin_config;
+    #if TTGO_TAUDIO
+            pin_config = {
+                .bck_io_num = 33,
+                .ws_io_num = 25,
+                .data_out_num = 26,
+                .data_in_num = 27
+            };
+    #elif A1S_BOARD
+            pin_config = {
+                .bck_io_num = 27,
+                .ws_io_num = 26,
+                .data_out_num = 25,
+                .data_in_num = 35
+            };
+    #else // Default
+            pin_config = {
+                .bck_io_num = 33,
+                .ws_io_num = 25,
+                .data_out_num = 26,
+                .data_in_num = 27
+            };
+    #endif
+            configureI2S(sample_rate, buffer_size, pin_config);
+            
+            if (fDSP.getNumInputs() > 0) {
+                fInChannel = new float*[fDSP.getNumInputs()];
+                for (int i = 0; i < fDSP.getNumInputs(); i++) {
+                    fInChannel[i] = new float[fBS];
                 }
             } else {
-                // otherwise only first channel
-                for (int i = 0; i < fBS; i++) {
-                    fInChannel[0][i] = (float)samples_data_in[i*2]*DIV_S32;
+                fInChannel = NULL;
+            }
+            
+            if (fDSP.getNumOutputs() > 0) {
+                fOutChannel = new float*[fDSP.getNumOutputs()];
+                for (int i = 0; i < fDSP.getNumOutputs(); i++) {
+                    fOutChannel[i] = new float[fBS];
                 }
+            } else {
+                fOutChannel = NULL;
             }
         }
-        
-        // Call DSP
-        fDSP->compute(fBS, fInChannel, fOutChannel);
-        
-        // Convert and copy outputs
-        int32_t samples_data_out[2*fBS];
-        if (OUTPUTS == 2) {
-            // if stereo
-            for (int i = 0; i < fBS; i++) {
-                samples_data_out[i*2] = clip(fOutChannel[0][i]);
-                samples_data_out[i*2+1] = clip(fOutChannel[1][i]);
-            }
-        } else {
-            // otherwise only first channel
-            for (int i = 0; i < fBS; i++) {
-                samples_data_out[i*2] = clip(fOutChannel[0][i]);
-                samples_data_out[i*2+1] = samples_data_out[i*2];
-            }
-        }
-        
-        // Write to the card
-        size_t bytes_written = 0;
-        i2s_write((i2s_port_t)0, &samples_data_out, 8*fBS, &bytes_written, portMAX_DELAY);
-    }
-}
 
-void AudioFaust::audioTaskHandler(void* arg)
-{
-    AudioFaust* audio = (AudioFaust*)arg;
-    audio->audioTask<FAUST_INPUTS, FAUST_OUTPUTS>();
-}
+        ~AudioFaust()
+        {
+            for (int i = 0; i < fDSP.getNumInputs(); i++) {
+                delete[] fInChannel[i];
+            }
+            delete [] fInChannel;
+            
+            for (int i = 0; i < fDSP.getNumOutputs(); i++) {
+                delete[] fOutChannel[i];
+            }
+            delete [] fOutChannel;
+        }
+    
+        bool start()
+        {
+            xTaskCreate(audioTaskHandler, "Faust DSP Task", 1024, (void*)this, 5, &fHandle);
+            return true;
+        }
+    
+        void stop()
+        {
+            if (fHandle != NULL) {
+                vTaskDelete(fHandle);
+                fHandle = NULL;
+            }
+        }
+        void setParamValue(const std::string& path, float value)
+        {
+            fUI.setParamValue(path, value);
+        }
+
+};
+
 
 /********************END ARCHITECTURE SECTION (part 2/2)****************/
